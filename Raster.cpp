4fde57b4133f83988ec907f16259868fc244bf52
Kidus-Sam <kidussamson2004@gmail.com>
2024-09-09 16:39:47 -0400

    Fixed most edge cases

--- Raster.cpp
+++ Raster.cpp
@@ -46,7 +46,7 @@ void Raster::clear(Color pFillColor){
 }
 
 
-//some code taken/adapted from video tutorial "" 
+//some code taken/adapted from video tutorial "https://www.youtube.com/watch?v=6byB1jX3Fnk" 
 void Raster::writeToPPM(){
     std::ofstream fout("FRAME_BUFFER.ppm");
     if(fout.fail()){std::cout<<"The file could not be opened."<<std::endl; exit(1);}
@@ -69,12 +69,12 @@ void Raster::drawLine_DDA(float x1, float y1, float x2, float y2, Color pFillCol
     float m = dy/dx;
     swap(x1,y1,x2,y2,m);
     check(x1,y1,x2,y2,m);
-    std::cout<<"m:"<<fabs(m)<<std::endl;
+    std::cout<<"m:"<<m<<std::endl;
     std::cout<<"X1: "<<x1<<" Y1: "<<y1<<" X2: "<<x2<<" Y2: "<< y2<<std::endl;
     if(fabs(m)<=1){
         std::cout<<"X:"<<x1<<std::endl;
         float y = y1;
-        for(int x=x1; x<=x2; x++){
+        for(int x=x1; x<=float(round(x2)); x++){
             setColorPixel(x, float(round(y)), pFillColor);
             y+=m;
         }
@@ -83,7 +83,7 @@ void Raster::drawLine_DDA(float x1, float y1, float x2, float y2, Color pFillCol
     else{
         float x=x1;
         m = dx/dy;
-        for(int y=y1; y>=y2; y--){
+        for(int y=y1; y>=float(round(y2)); y--){
             setColorPixel(float(round(x)), y, pFillColor);
             x-=m;
         }
@@ -100,7 +100,7 @@ void Raster::swap(float& x1, float& y1, float& x2, float& y2, float m){
         }
     }
     else{
-        std::cout<<"X1:"<<x1<<std::endl;
+        std::cout<<"slope is less than 1"<<std::endl;
         if(x1>x2){
             std::swap(x1,x2);
             std::swap(y1,y2);
@@ -117,17 +117,29 @@ void Raster::check(float& x1, float& y1, float& x2, float& y2, float m){
     if(fabs(m)<=1){
         yIntercept= y1 - m*x1;
         xIntercept= -yIntercept/m;
-        if(x1<0&&y1>0){
+        std::cout<<"xintercept:"<<xIntercept<<std::endl;
+        std::cout<<"yintercept:"<<yIntercept<<std::endl;
+        if(x1<0 && y1>0){
             x1=0;
             y1= yIntercept;
             std::cout<<"x1 is less than 0 and y1 is greater than 0"<<std::endl;
         }
-        if(x2>width&&y2<0){
-            x2=width;
+        if(x1<0&&y1<0&&yIntercept>=0){
+            x1=0;
+            y1= yIntercept;
+            std::cout<<"x1 is less than 0 and y1 is less than 0 and Xintercept is greater than 0"<<std::endl;
+        }
+        if(x1<0&&y1<0&&yIntercept<=0){
+            x1=xIntercept;
+            y1= 0;
+            std::cout<<"x1 is less than 0 and y1 is less than 0 and Xintercept is less than 0"<<std::endl;
+        }
+        if(x2>=width&&y2<0){
+            x2=width-1;
             y2= m*width + yIntercept;
             std::cout<<"x2 is greater than width and y2 is less than 0"<<std::endl;
         }
-        if(y1>height&&x1<0){
+        if(y1>=height&&x1<0){
             y1=height;
             x1= (height-yIntercept)/m;
             std::cout<<"y1 is greater than height and x1 is less than 0"<<std::endl;
@@ -137,13 +149,13 @@ void Raster::check(float& x1, float& y1, float& x2, float& y2, float m){
             x2= (0-yIntercept)/m;
             std::cout<<"y2 is less than 0 and x2 is less than width"<<std::endl;
         }
-        if(x2>width&&y2<=height){
-            x2=width;
+        if(x2>=width&&y2<=height){
+            x2=width-1;
             y2= m*width + yIntercept;
             std::cout<<"x2 is greater than width and y2 is less than or equal to height"<<std::endl;
         }
-        if(x2>width&&y2>height){
-            x2=width;
+        if(x2>=width&&y2>=height){
+            x2=width-1;
             y2=m*width + yIntercept;
             std::cout<<"x2 is greater than width and y2 is greater than height"<<std::endl;
         }
@@ -155,6 +167,7 @@ void Raster::check(float& x1, float& y1, float& x2, float& y2, float m){
         yIntercept= -xIntercept/m;
         std::cout<<"xintercept:"<<xIntercept<<std::endl;
         std::cout<<"yintercept:"<<yIntercept<<std::endl;
+        std::cout<<"m-corrected:"<<m<<std::endl;
         if(x2<0&&y2>0&&xIntercept>=0){
             x2= xIntercept;
             y2=0;
@@ -165,12 +178,22 @@ void Raster::check(float& x1, float& y1, float& x2, float& y2, float m){
             y2= yIntercept;
             std::cout<<"x2 is less than 0 and y2 is greater than 0 and Xintercept is less than 0"<<std::endl;
         }
-        if(x1>width&&y1<0){
-            x1= xIntercept;
-            y1=0;
+        if(x2<0&&y2<0&&yIntercept>=0){
+            x2=0;
+            y2= yIntercept;
+            std::cout<<"x2 is less than 0 and y2 is less than 0 and the yintercept is greater than 0"<<std::endl;
+        }
+        else if(x2<0&&y2<0&&yIntercept<0){
+            x2= xIntercept;
+            y2=0;
+            std::cout<<"x2 is less than 0 and y2 is less than 0 and yintercept is less than 0"<<std::endl;
+        }
+        if(x2>=width&&y2<0){
+            x2= width-1;
+            y2= (x2-xIntercept)/m;
             std::cout<<"x1 is greater than width and y1 is less than 0"<<std::endl;
         }
-        if(y2>height&&x2<0){
+        if(y2>=height&&x2<0){
             y2=height;
             x2= m*height + xIntercept;
             std::cout<<"y2 is greater than height and x2 is less than 0"<<std::endl;
@@ -180,5 +203,16 @@ void Raster::check(float& x1, float& y1, float& x2, float& y2, float m){
             x1= m*height + xIntercept;
             std::cout<<"y1 is less than 0 and x1 is less than width"<<std::endl;
         }
+        if(y1>=height&&x1>=width&&(m*height + xIntercept)<height){
+            y1=height;
+            x1= m*height + xIntercept;
+            std::cout<<"y1 is greater than height and x1 is greater than width"<<std::endl;
+        }
+        if(y1>=height&&x1>=width&&(m*height + xIntercept)>height){
+            y1=(height-xIntercept)/m;
+            x1=width-1;
+            std::cout<<"y1 is greater than height and x1 is greater than width"<<std::endl;
+        }
+
     }
 }
