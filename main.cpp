ea67811a610629b390713b063aa4955b2d37f694
Kidus-Sam <kidussamson2004@gmail.com>
2024-09-11 00:47:26 -0400

    Fix the DDA line Algorithm to handle negative coordinates properly

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
     int main()
     {
         Raster copy= Raster(12,12,Green);
-        copy.drawLine_DDA(-2,-2,5,5,Red);
+        copy.drawLine_DDA(1,8,-5,15,Red);
+        copy.drawLine_DDA(3,8,-5,15,Red);
+        copy.drawLine_DDA(5,9,-5,15,Red);
         copy.writeToPPM();
         }
